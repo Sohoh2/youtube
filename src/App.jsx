77c15d9b1930171acfc7486be6378fe959500595
suuuuhyun <79616760+suuuuhyun@users.noreply.github.com>
2021-03-12 11:35:37 +0900

    commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './app.css';
+
+
+function App() {
+  const name = 'ellie';
+  return (
+    <>
+    <h1>Hello {name} :)</h1>
+    <h1>Hello</h1>
+    </>
+  );
+}
+
+export default App;
